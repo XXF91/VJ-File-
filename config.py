0ca56042281fe15e7a59e32e4b55e74f12ae5f17
XXF91 <mrsjoaker@gmail.com>
2025-01-19 00:40:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -20,22 +20,22 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "23416113"))
 API_HASH = environ.get("API_HASH", "5f66046e7129c9bf6e2b3da943ae2993")
-BOT_TOKEN = environ.get("BOT_TOKEN", ":-")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://l.top4top.io/p_3305ca8df0.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6662808885').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "UrlUploadersX2Bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://puneeth974323:IeB05zCkDaG37c8l@cluster0.6l3bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://puneeth974323:IeB05zCkDaG37c8l@cluster0.6l3bc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002010829644"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002229217917"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
